888673a38e3f3883082f8e3f6a8baa727bb87c8e
Michal Jankowski <mjankowski@frontendsolutions.pl>
2026-08-25 09:08:23 +0200

    chore(docker): add test db

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vitest/config";
 
-process.env.DATABASE_URL = "postgresql://test:test@localhost:5432/test_db";
+process.env.DATABASE_URL = "postgresql://test_user:test_pass@localhost:5433/test_db";
 
 export default defineConfig({});
 
